7a5e92844d2f0b8deda1d362a1f6b3b9f73b2c62
EC2 Default User <ec2-user@ip-172-31-35-172.us-east-2.compute.internal>
2020-03-12 03:23:03 +0000

    AutomatedCommitAndPush

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-const express = require('express')
-const path = require('path');
-const app = express()
-const port = process.env.PORT || 8080;
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
+// set the template engine to ejs
+app.set('view engine', 'ejs');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// Tells node to look in css/ for styles
+app.use(express.static("css"));
+
+// makes data that comes to the server from the client a json object
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.render("home.ejs");
+});
+
+
+app.get("/*", function(req, res){
+    res.render("error.ejs");
+});
+
+app.listen(process.env.PORT || 3000 || 8080, function(){
+    console.log("Server is running");
+});

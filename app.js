2f912259fb7974874bc9518be6789dfc2efd184a
EC2 Default User <ec2-user@ip-172-31-35-172.us-east-2.compute.internal>
2020-03-12 04:04:06 +0000

    AutomatedCommitAndPush

--- app.js
+++ app.js
@@ -5,21 +5,28 @@ var app = express();
 // set the template engine to ejs
 app.set('view engine', 'ejs');
 
-// Tells node to look in css/ for styles
-app.use(express.static("css"));
+// Tells node to look in public/ for styles
+app.use(express.static("public"));
 
 // makes data that comes to the server from the client a json object
 app.use(bodyParser.urlencoded({extended: true}));
 
+// route to base domain
 app.get("/", function(req, res){
     res.render("home.ejs");
 });
 
+app.get("/login", function(req, res){
+    res.render("login.ejs");
+});
 
+// anything that hasn't matched a defined route is caught here
 app.get("/*", function(req, res){
     res.render("error.ejs");
 });
 
+// listens for http requests
+// port 3000 is for C9, port 8080 is for Heroku
 app.listen(process.env.PORT || 3000 || 8080, function(){
     console.log("Server is running");
 });

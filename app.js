928300fed364b7fefee33fdeefe8912bda5120bb
EC2 Default User <ec2-user@ip-172-31-35-172.us-east-2.compute.internal>
2020-03-27 23:53:02 +0000

    home page now has otter content

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
-
+const axios = require('axios');
 
 // set the template engine to ejs
 app.set('view engine', 'ejs');
@@ -13,14 +13,41 @@ console.log(__dirname);
 // makes data that comes to the server from the client a json object
 app.use(bodyParser.urlencoded({extended: true}));
 
+// gets data based on search term from omdb
+// its the same function as the one in search.js
+// importing is a pain in the ass in node
+function get_omdb_data_from_search_term(searchTerm){
+    var apiKey = "40129ad8";
+    var url = "http://www.omdbapi.com/?apikey=" + apiKey + "&s=" + searchTerm;
+    
+    console.log("Requesting OMDB s=" + searchTerm);
+    console.log("url: " + url);
+    
+    return axios.get(url)
+        .then(function (response) {
+            return response.data;
+        })
+        .catch(function (error) {
+            console.log("Error Fetching Data from OMDB");
+            return error;
+        })
+        .finally(function () {
+            // always executed
+    });
+}
+
 // route to base domain
 app.get("/", function(req, res){
-    res.render("index.ejs", { data : false });
+    get_omdb_data_from_search_term("otter").then(function(data){
+        res.render("index.ejs", { data : data } );
+    });
 });
 
 // route to base domain
 app.get("/home", function(req, res){
-    res.render("index.ejs", { data : false });
+    get_omdb_data_from_search_term("otter").then(function(data){
+        res.render("index.ejs", { data : data } );
+    });
 });
 
 

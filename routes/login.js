0f944f137a0d53b14a3f4b67b6f4b74c4b55a38b
EC2 Default User <ec2-user@ip-172-31-35-172.us-east-2.compute.internal>
2020-03-27 03:37:48 +0000

    AutomatedCommitAndPush

--- routes/login.js
+++ routes/login.js
@@ -4,16 +4,19 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    res.render('login.ejs');
     console.log("login GET");
+    res.render('login.ejs');
 });
 
 
+// here is where we can validate a user login, log them in if valid and return true
+// or don't log them in if invalid and return false
 function validate_login(username, password){
-    return (Math.floor(Math.random() * 2) == 0) ? true : false;
+    return Math.floor(Math.random() * 2) == 0;
 }
+
 router.post('/attempt', function(req, res) {
-    console.log("login POST");
+    console.log("login/attempt POST");
     var username = req.body.username;
     var password = req.body.password;
     
@@ -21,6 +24,31 @@ router.post('/attempt', function(req, res) {
     console.log(password);
     res.json(
         { success:validate_login(username, password) }
+    );
+});
+
+
+function validate_new_user(username, password){
+    return Math.floor(Math.random() * 2) == 0;
+}
+
+router.get('/create', function(req, res){
+    console.log('login/create GET');
+    res.render('createAccount.ejs');
+});
+
+router.post('/create/attempt', function(req, res){
+    console.log("login/create/attempt POST");
+    var fullname = req.body.fullname;
+    var username = req.body.username;
+    var password = req.body.password;
+    var passconf = req.body.passwordConfirm;
+    console.log(username);
+    console.log(password);
+    console.log(fullname);
+    console.log(passconf);
+    res.json(
+        { success: validate_new_user(username, password) }  
     );
 });
 
